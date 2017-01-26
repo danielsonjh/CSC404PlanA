af579d84b1cd50c9d1a3fa10b427eab648f9ecbd
Daniel Son <danielsonjh@hotmail.com>
2017-01-26 15:39:43 -0500

    Added strobe light.

---
+++ Assets/TimeController/LightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    private const float FadeDuration = 0.2f;
+
+    private Light _light;
+
+    void Start()
+    {
+        _light = GetComponent<Light>();
+
+        TimeController.Instance.OnToggle += Fade;
+    }
+
+    void Fade()
+    {
+        StartCoroutine(TimeController.Instance.IsMove ? FadeCoroutine(1) : FadeCoroutine(0.7f));
+    }
+
+    IEnumerator FadeCoroutine(float target)
+    {
+        var origIntensity = _light.intensity;
+        var t = 0f;
+        while (t <= 1)
+        {
+            _light.intensity = Mathf.Lerp(origIntensity, target, t);
+            yield return new WaitForEndOfFrame();
+            t += TimeController.Instance.DeltaTime / FadeDuration;
+        }
+        _light.intensity = target;
+    }
+}
